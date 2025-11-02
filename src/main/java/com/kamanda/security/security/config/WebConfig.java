c50909098c28e7215de02acd6b76aee480e717b8
usifu <usifubarriem@gmail.com>
2025-11-02 15:51:12 +0000

    Update database config for Railway

--- src/main/java/com/kamanda/security/security/config/WebConfig.java
+++ src/main/java/com/kamanda/security/security/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
